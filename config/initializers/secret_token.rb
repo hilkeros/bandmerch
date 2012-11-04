d7e88e44d135e46937357acdd648497df19b2dc6
Hilke Ros <hilke@mmmotion.com>
2012-11-04 16:54:15 +0100

    internationalization?

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bandmerch::Application.config.secret_token = '8fcab58044a37389c96b14c47104f7b38c6b0d6d1d16eb9aa2f7792781a8e72145b473455de07b358bfadebf033f04d20a1d5be6ebe321285eda63c9d8510e06'
